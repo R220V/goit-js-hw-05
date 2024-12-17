85c647fa0d003d20482c0f6de75be427ad9e4ec7
Krus <krus.margo@gmail.com>
2024-12-17 22:48:31 +0200

    corrected hw5

--- js/task-2.js
+++ js/task-2.js
@@ -1,69 +1,69 @@
-// Задача 2. Користувачі з другом
+// // Задача 2. Користувачі з другом
 
-// Напиши стрілочну функцію getUsersWithFriend(users, friendName) , яка прийматиме два параметра:
+// // Напиши стрілочну функцію getUsersWithFriend(users, friendName) , яка прийматиме два параметра:
 
-// перший параметр users — масив об’єктів користувачів
-// другий параметр friendName — ім’я друга для пошуку.
-// Функція має повертати масив усіх користувачів із масиву users, у яких є друг з іменем friendName.
-// Друзі кожного користувача зберігаються у властивості friends.
-//  Якщо користувачів, у яких є такий друг немає, то функція має повернути порожній масив.
+// // перший параметр users — масив об’єктів користувачів
+// // другий параметр friendName — ім’я друга для пошуку.
+// // Функція має повертати масив усіх користувачів із масиву users, у яких є друг з іменем friendName.
+// // Друзі кожного користувача зберігаються у властивості friends.
+// //  Якщо користувачів, у яких є такий друг немає, то функція має повернути порожній масив.
 
-const getUsersWithFriend = (users, friendName) =>
-  users.filter(user => user.friends.includes(friendName));
+// const getUsersWithFriend = (users, friendName) =>
+//   users.filter(user => user.friends.includes(friendName));
 
-const allUsers = [
-  {
-    name: 'Moore Hensley',
-    friends: ['Sharron Pace'],
-  },
-  {
-    name: 'Sharlene Bush',
-    friends: ['Briana Decker', 'Sharron Pace'],
-  },
-  {
-    name: 'Ross Vazquez',
-    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-  },
-  {
-    name: 'Elma Head',
-    friends: ['Goldie Gentry', 'Aisha Tran'],
-  },
-  {
-    name: 'Carey Barr',
-    friends: ['Jordan Sampson', 'Eddie Strong'],
-  },
-  {
-    name: 'Blackburn Dotson',
-    friends: ['Jacklyn Lucas', 'Linda Chapman'],
-  },
-  {
-    name: 'Sheree Anthony',
-    friends: ['Goldie Gentry', 'Briana Decker'],
-  },
-];
-
-console.log(getUsersWithFriend(allUsers, 'Briana Decker'));
-// [
+// const allUsers = [
 //   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"]
+//     name: 'Moore Hensley',
+//     friends: ['Sharron Pace'],
 //   },
 //   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
-
-console.log(getUsersWithFriend(allUsers, 'Goldie Gentry'));
-// [
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"]
+//     name: 'Sharlene Bush',
+//     friends: ['Briana Decker', 'Sharron Pace'],
 //   },
 //   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"]
-//   }
-// ]
+//     name: 'Ross Vazquez',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//   },
+//   {
+//     name: 'Elma Head',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//   },
+//   {
+//     name: 'Carey Barr',
+//     friends: ['Jordan Sampson', 'Eddie Strong'],
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//   },
+// ];
 
-console.log(getUsersWithFriend(allUsers, 'Adrian Cross')); // []
+// console.log(getUsersWithFriend(allUsers, 'Briana Decker'));
+// // [
+// //   {
+// //     name: "Sharlene Bush",
+// //     friends: ["Briana Decker", "Sharron Pace"]
+// //   },
+// //   {
+// //     name: "Sheree Anthony",
+// //     friends: ["Goldie Gentry", "Briana Decker"]
+// //   }
+// // ]
+
+// console.log(getUsersWithFriend(allUsers, 'Goldie Gentry'));
+// // [
+// //   {
+// //     name: "Elma Head",
+// //     friends: ["Goldie Gentry", "Aisha Tran"]
+// //   },
+// //   {
+// //     name: "Sheree Anthony",
+// //     friends: ["Goldie Gentry", "Briana Decker"]
+// //   }
+// // ]
+
+// console.log(getUsersWithFriend(allUsers, 'Adrian Cross')); // []
--- js/task-3.js
+++ js/task-3.js
@@ -6,8 +6,12 @@
 
 // ?Для перебирання параметра users використаний метод toSorted()
 
-const sortByDescendingFriendCount = users =>
-  users.sort((a, b) => b.friends.length - a.friends.length);
+// const sortByDescendingFriendCount = users =>
+//   users.sort((a, b) => b.friends.length - a.friends.length);
+
+const sortByDescendingFriendCount = users => {
+  return users.toSorted((a, b) => b.friends.length - a.friends.length);
+};
 
 console.log(
   sortByDescendingFriendCount([
